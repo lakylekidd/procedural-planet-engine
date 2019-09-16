5f744159b6992cbeb577bff770f13ae471b1b74e
Billy Vlachos <williamcaminiti@live.com>
2019-09-17 00:33:41 +0200

    Include additional layers in noise filter

--- Assets/NoiseFilter.cs
+++ Assets/NoiseFilter.cs
@@ -13,7 +13,18 @@ public class NoiseFilter
     public float Evaluate(Vector3 point)
     {
         // Retrieve a value from the point and convert to range 0 - 1
-        float noiseValue = (noise.Evaluate(point * settings.roughness + settings.center) + 1) * .5f;
+        float noiseValue = 0;
+        float frequency = settings.baseRoughness;
+        float amplitude = 1;
+        for (int i = 0; i < settings.numLayers; i++)
+        {
+            // Calculate the noise value 
+            float v = noise.Evaluate(point * frequency + settings.center);
+            noiseValue += (v + 1) * .5f * amplitude;
+            // Determine the frequency and persistence for each layer
+            frequency *= settings.roughness;
+            amplitude *= settings.persistence;
+        }
         return noiseValue * settings.strength;
     }
 }
