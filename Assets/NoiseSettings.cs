aa9cc243a7a21f7b6ece571d471bc8d9bac3dd8e
Billy Vlachos <williamcaminiti@live.com>
2019-09-17 00:30:03 +0200

    Include additional properties in the noise settings script

--- Assets/NoiseSettings.cs
+++ Assets/NoiseSettings.cs
@@ -4,6 +4,10 @@
 public class NoiseSettings
 {
     public float strength = 1;
-    public float roughness = 1;
+    [Range(1, 8)]
+    public int numLayers = 1;
+    public float baseRoughness = 1;
+    public float roughness = 2;
+    public float persistence = .5f;
     public Vector3 center;
 }
