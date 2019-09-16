87bb69f8b7085ea7798f7f010cbcd60aaaff20f8
Billy Vlachos <williamcaminiti@live.com>
2019-09-17 00:48:56 +0200

    Include multiple noise layers

--- Assets/ShapeGenerator.cs
+++ Assets/ShapeGenerator.cs
@@ -3,17 +3,27 @@
 public class ShapeGenerator
 {
     private ShapeSettings settings;
-    private NoiseFilter noiseFilter;
+    private NoiseFilter[] noiseFilters;
 
     public ShapeGenerator(ShapeSettings settings)
     {
         this.settings = settings;
-        noiseFilter = new NoiseFilter(settings.noiseSettings);
+        noiseFilters = new NoiseFilter[settings.noiseLayers.Length];
+
+        for (int i = 0; i < noiseFilters.Length; i++)
+        {
+            noiseFilters[i] = new NoiseFilter(settings.noiseLayers[i].noiseSettings);
+        }
     } 
 
     public Vector3 CalculatePointOnSphere(Vector3 pointOnUnitSphere)
     {
-        float elevation = noiseFilter.Evaluate(pointOnUnitSphere);
+        float elevation = 0;
+        for (int i = 0; i < noiseFilters.Length; i++)
+        {
+            if (settings.noiseLayers[i].enabled)
+                elevation += noiseFilters[i].Evaluate(pointOnUnitSphere);
+        }
         return pointOnUnitSphere * settings.planetRadius * (1 + elevation);
     }
 }
--- Assets/ShapeSettings.cs
+++ Assets/ShapeSettings.cs
@@ -4,5 +4,12 @@
 public class ShapeSettings : ScriptableObject
 {
     public float planetRadius = 1;
-    public NoiseSettings noiseSettings;
+    public NoiseLayer[] noiseLayers;
+
+    [System.Serializable]
+    public class NoiseLayer
+    {
+        public bool enabled = true;
+        public NoiseSettings noiseSettings;
+    }
 }
