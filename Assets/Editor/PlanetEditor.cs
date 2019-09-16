9e62c87b454ba070b17caf195507f96eb579dcaf
Billy Vlachos <williamcaminiti@live.com>
2019-09-16 23:39:42 +0200

    Show shape and color settings in inspector

--- Assets/Editor/PlanetEditor.cs
+++ Assets/Editor/PlanetEditor.cs
@@ -1,7 +1,28 @@
-﻿using UnityEditor;
+﻿using UnityEngine;
+using UnityEditor;
 
 [CustomEditor(typeof(Planet))]
 public class PlanetEditor : Editor
 {
+    private Planet planet;
 
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        DrawSettingsEditor(planet.shapeSettings);
+        DrawSettingsEditor(planet.colorSettings);
+    }
+
+    private void DrawSettingsEditor(Object settings)
+    {
+        Editor editor = CreateEditor(settings);
+        editor.OnInspectorGUI();
+    }
+
+    private void OnEnable()
+    {
+        planet = (Planet)target;
+    }
+
+    
 }
