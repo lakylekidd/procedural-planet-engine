697c93e60da4b49e39c2867af5dcc0ebeaa3562d
Billy Vlachos <williamcaminiti@live.com>
2019-09-17 00:19:52 +0200

    Implement noise filter and include it in shape generator

--- Assets/NoiseFilter.cs
+++ Assets/NoiseFilter.cs
@@ -1,4 +1,13 @@
-﻿public class NoiseFilter
+﻿using UnityEngine;
+
+public class NoiseFilter
 {
-    
+    Noise noise = new Noise();
+
+    public float Evaluate(Vector3 point)
+    {
+        // Retrieve a value from the point and convert to range 0 - 1
+        float noiseValue = (noise.Evaluate(point) + 1) * .5f;
+        return noiseValue;
+    }
 }
--- Assets/ShapeGenerator.cs
+++ Assets/ShapeGenerator.cs
@@ -1,14 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ShapeGenerator
 {
     private ShapeSettings settings;
+    private NoiseFilter noiseFilter;
 
     public ShapeGenerator(ShapeSettings settings)
     {
         this.settings = settings;
+        noiseFilter = new NoiseFilter();
     } 
 
     public Vector3 CalculatePointOnSphere(Vector3 pointOnUnitSphere)
