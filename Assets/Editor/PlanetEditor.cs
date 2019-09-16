91947faf66c8a2b0964e81838ba0a0534ebd91ca
Billy Vlachos <williamcaminiti@live.com>
2019-09-17 00:00:12 +0200

    Refactor so that we don't create new editor for each change

--- Assets/Editor/PlanetEditor.cs
+++ Assets/Editor/PlanetEditor.cs
@@ -5,25 +5,31 @@ using UnityEditor;
 public class PlanetEditor : Editor
 {
     private Planet planet;
+    private Editor shapeEditor;
+    private Editor colorEditor;
 
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
-        DrawSettingsEditor(planet.shapeSettings, planet.OnShapeSettingsUpdated, ref planet.shapeSettingsFoldout);
-        DrawSettingsEditor(planet.colorSettings, planet.OnColorSettingsUpdated, ref planet.colorSettingsFoldout);
+        DrawSettingsEditor(planet.shapeSettings, planet.OnShapeSettingsUpdated, ref planet.shapeSettingsFoldout, ref shapeEditor);
+        DrawSettingsEditor(planet.colorSettings, planet.OnColorSettingsUpdated, ref planet.colorSettingsFoldout, ref colorEditor);
     }
 
-    private void DrawSettingsEditor(Object settings, System.Action onSettingsUpdated, ref bool foldout)
+    private void DrawSettingsEditor(Object settings, System.Action onSettingsUpdated, ref bool foldout, ref Editor editor)
     {
-        foldout = EditorGUILayout.InspectorTitlebar(foldout, settings);
-
-        if (foldout)
+        if (settings != null)
         {
-            Editor editor = CreateEditor(settings);
-            editor.OnInspectorGUI();
-        }
-                
-        onSettingsUpdated?.Invoke();
+            foldout = EditorGUILayout.InspectorTitlebar(foldout, settings);
+
+            if (foldout)
+            {
+                CreateCachedEditor(settings, null, ref editor);
+                editor = CreateEditor(settings);
+                editor.OnInspectorGUI();
+            }
+
+            onSettingsUpdated?.Invoke();
+        }        
     }
 
     private void OnEnable()
